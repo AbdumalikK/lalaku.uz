bbb7744adfcce8d9b1c46de86f45c3faa37ebede
Abdumalik Karimov <uzbekistanes2001@gmailcom>
2020-12-08 22:54:51 +0500

    Initial commit

---
+++ style/js/index.js
@@ -0,0 +1,8 @@
+
+ const burger = document.querySelector('.burger');
+ const mainNav = document.querySelector('.main-nav');
+
+ burger.addEventListener('click', () => {
+   mainNav.classList.toggle('nav-active');
+ });
+
